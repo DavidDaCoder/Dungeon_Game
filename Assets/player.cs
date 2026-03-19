95525410de10f2c3bca6ea691a5ee5e0a1787aa7
ComicSansM5 <140101852+ComicSansM5@users.noreply.github.com>
2026-03-19 17:17:54 +0900

    Added proper collision and deathboxes

--- Assets/player.cs
+++ Assets/player.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Interactions;
+using UnityEngine.Video;
 public class player : MonoBehaviour
 {
     public LayerMask groundLayer;
+    public LayerMask hazardLayer;
+    [SerializeField] private VideoPlayer vd;
+    [SerializeField] private AudioSource ad;
     [SerializeField] private float speed;
     [SerializeField] private float jumpStrength;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private int doubleJumps;
     [SerializeField] private float coyoteTime;
-    [SerializeField] private Vector2 boxSize;
+    [SerializeField] private Vector2 groundedCheckWidth;
+    [SerializeField] private Vector2 wallCheckWidth;
     [SerializeField] private float castDistance;
     private float coyoteTimer;
     private int doubleJumpCount = 0;
@@ -22,20 +27,31 @@ public class player : MonoBehaviour
         
     }
 
+    public void DIE()
+    {
+        ad.Play();
+        vd.Play();
+    }
+
     // used to check bounds of grounded check hitbox
     /*
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireCube(transform.position-transform.up*castDistance, boxSize);
+        Gizmos.DrawWireCube(transform.position-transform.up*castDistance, groundedCheckWidth);
     }
     */
     // Update is called once per frame
     void Update()
     {
-        grounded = Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer);
+        grounded = Physics2D.BoxCast(transform.position, groundedCheckWidth, 0, -transform.up, castDistance, groundLayer);
 
+        if (Physics2D.BoxCast(transform.position, new Vector2(.7f,.7f), 0, transform.up, 0, hazardLayer))
+        {
+            DIE();
+        }
         if (grounded)
         {
+            doubleJumpCount = 0;
             coyoteTimer = 0;
         }
         else
@@ -43,16 +59,24 @@ public class player : MonoBehaviour
             coyoteTimer++;
         }
         rb.linearVelocity = new Vector2 (xVelocity * speed, rb.linearVelocityY);
-        //FIX THIS DAVID MAKE IT A BOXCAST SO THAT IT CAN CHECK FOR IF THE VELOCITY VECTOR WOULD SHOVE IT INTO A WALL SO THAT YOU CAN NO LONGER WALL CLING
-        if (Physics2D.Raycast(transform.position, new Vector2(rb.linearVelocityX,0), rb.linearVelocityX, groundLayer))
+        
+        if (rb.linearVelocityX > 0 && Physics2D.BoxCast(transform.position + new Vector3(0,.05f), wallCheckWidth, 0, transform.right, .18f, groundLayer))
         {
             rb.linearVelocity = new Vector2(0, rb.linearVelocityY);
         }
-  
-
-
+        else if (rb.linearVelocityX < 0 && Physics2D.BoxCast(transform.position + new Vector3(0, .05f), wallCheckWidth, 0, -transform.right, .22f, groundLayer))
+        {
+            rb.linearVelocity = new Vector2(0, rb.linearVelocityY);
+        }
     }
-
+    /*
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position + new Vector3(.18f,0f), wallCheckWidth);
+        Gizmos.DrawWireCube(transform.position - new Vector3(.22f, 0f), wallCheckWidth);
+    }
+    */
+    
     public void Move(InputAction.CallbackContext context)
     {
         xVelocity = context.ReadValue<Vector2>().x;
